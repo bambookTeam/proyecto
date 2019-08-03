ab29fd756006e06f95247504f29d73e949eec8bc
rarebeatle <amoralesm@ucenfotec.ac.cr>
2019-08-02 18:00:04 -0600

    Commit Ari

---
+++ base/public/js/servicio_admingene.js
@@ -0,0 +1,44 @@
+'use strict';
+
+let registrarContacto = (pnombre, pcorreo) => {
+    axios({
+        method: 'post',
+        url: 'http://localhost:4000/api/registrar-contacto',
+        responseType: 'json',
+        data: {
+            nombre: pnombre,
+            correo: pcorreo,
+            
+        }
+    });
+};
+
+let obtenerContactos = async() => {
+    try {
+        // fetch data from an url endpoint
+        const response = await axios({
+            method: 'get',
+            url: 'http://localhost:4000/api/listar-contactos',
+            responseType: 'json'
+        });
+
+        return response.data.lista_contactos;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+let obtenerContactoId = async(_id) => {
+    try {
+        // fetch data from an url endpoint
+        const response = await axios({
+            method: 'get',
+            url: `http://localhost:4000/api/buscar-contacto-id/${_id}`,
+            responseType: 'json'
+        });
+
+        return response.data.contacto;
+    } catch (error) {
+        console.log(error);
+    }
+};
