c0ce750b3cc7112d6f12dc867ad5cd29b65ab776
Seb Chacon Rodriguez <zebaz9898@gmail.com>
2019-08-03 20:11:37 -0600

    correccion axel

--- base/public/js/controlador_listar_libros.js
+++ base/public/js/controlador_listar_libros.js
@@ -15,17 +15,17 @@ let mostrar_tabla = async () =>{
     for (let i=0; i<lista_libros.length; i++){
         let fila = tbody.insertRow();
         fila.insertCell().innerHTML = lista_libros[i]['titulo'];
-        fila.insertCell().innerHTML = lista_libros[i]['edición'];
+        fila.insertCell().innerHTML = lista_libros[i]['edicion'];
         fila.insertCell().innerHTML = lista_libros[i]['editorial'];
         fila.insertCell().innerHTML = lista_libros[i]['autor'];
-        fila.insertCell().innerHTML = lista_libros[i]['año'];
+        fila.insertCell().innerHTML = lista_libros[i]['anno'];
         fila.insertCell().innerHTML = lista_libros[i]['idioma'];
         fila.insertCell().innerHTML = lista_libros[i]['isbn'];
         fila.insertCell().innerHTML = lista_libros[i]['genero'];
         fila.insertCell().innerHTML = lista_libros[i]['tipo'];
-        fila.insertCell().innerHTML = lista_libros[i]['existencia'];
+        fila.insertCell().innerHTML = lista_libros[i]['cantidad'];
         fila.insertCell().innerHTML = lista_libros[i]['precio'];
-        fila.insertCell().innerHTML = lista_libros[i]['portada'];
+        fila.insertCell().innerHTML = lista_libros[i]['imagen'];
 
     }
 }
