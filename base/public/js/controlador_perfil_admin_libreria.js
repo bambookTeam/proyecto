84c8a5d6e0a5132e8086c8b4c6569aeda24b4e6f
bambookTeam <52986437+bambookTeam@users.noreply.github.com>
2019-08-05 01:41:44 -0600

    Naty

--- base/public/js/controlador_perfil_admin_libreria.js
+++ base/public/js/controlador_perfil_admin_libreria.js
@@ -4,32 +4,20 @@ const urlParams = new URLSearchParams(window.location.search);
 
 let _id = urlParams.get('_id');
 
-primerNombre: {type: String, required: true,unique: false},
-  segundoNombre: {type: String, requiered: false, unique: false},
-  primerApellido: {type: String, required: true,unique: false},
-  segundoApellido:
-
-const identificacion = document.querySelector('#txt_nombre');
-const primerNombre = document.querySelector('#txt_correo');
-const segundoNombre = document.querySelector('#txt_contraseña');
-const primerApellido = document.querySelector('#avatar');
-const segundoApellido = document.querySelector('#avatar');
-
-const primerApellido = document.querySelector('#avatar');
-const primerApellido = document.querySelector('#avatar');
-const primerApellido = document.querySelector('#avatar');
-const primerApellido = document.querySelector('#avatar');
-const primerApellido = document.querySelector('#avatar');
-
-
+const nombreUsuario = document.querySelector('#txt_nombre_usuario');
+const identificacion = document.querySelector('#txt_identificacion');
+const nombre = document.querySelector('#txt_nombre');
+const sexo = document.querySelector('#txt_sexo');
+const correo = document.querySelector('#txt_correo');
 
 let llenar_perfil = async() => {
     let usuario = await obtenerUsuarioId(_id);
     if (usuario) {
-        avatar.innerHTML = usuario['avatar'];
-        txt_nombre.innerHTML = contacto['nombre'];
-        txt_correo.innerHTML = contacto['correo'];
-        txt_contraseña.innerHTML=contacto['contraseña'];
+        nombreUsuario.innerHTML = usuario['nombreUsuario'];
+        identificacion.innerHTML = contacto['identificacion'];
+        nombre.innerHTML = contacto['primerNombre'];
+        sexo.innerHTML=contacto['sexo'];
+        correo.innerHTML=contacto['correo'];
     }
 };
 
