2d687b444dbe7b3db82997e81a5f975012b3de60
rarebeatle <amoralesm@ucenfotec.ac.cr>
2019-08-05 01:41:00 -0600

    Commit Ari

--- base/public/js/controlador_listar_tarjetas.js
+++ base/public/js/controlador_listar_tarjetas.js
@@ -4,36 +4,37 @@ let listar_tarjetas = [];
 let mostrarlista = async () => {
         listar_tarjetas = await obtenerTarjetas();
 
-        for (let index = 0; index < listar_tarjetas.length; index++) {
+//         for (let index = 0; index < listar_tarjetas.length; index++) {
 
         
-        let cardContainer = document.querySelector("#card-container");
+//         let cardContainer = document.querySelector("#card-container");
 
-       let cardDiv =  document.createElement("div") ;
-       cardDiv.classList.add("card-style");
+//        let cardDiv =  document.createElement("div") ;
+//        cardDiv.classList.add("card-style");
 
-       let title = document.createElement("h6");
-       title.innerText = listar_tarjetas[index]['numerotarjeta'];
+//        let title = document.createElement("h6");
+//        title.innerText = listar_tarjetas[index]['numerotarjeta'];
 
 
-       cardDiv.appendChild(title);
-       cardContainer.appendChild(cardDiv);
+//        cardDiv.appendChild(title);
+//        cardContainer.appendChild(cardDiv);
+tbody.innerHTML = '';
+
+for (let index = 0; index < listar_tarjetas.length; index++) {
+    let fila = tbody.insertRow();
+    fila.insertCell().innerHTML = listar_tarjetas[index]['numerotarjeta'];
+    fila.insertCell().innerHTML = listar_tarjetas[index]['fechavencimiento'];
+    fila.insertCell().innerHTML = "***";
+           
+ }
 }
 //     listar_tarjetas = await obtenerTarjetas();
-//     tbody.innerHTML = '';
 
-//     for (let index = 0; index < listar_tarjetas.length; index++) {
-//         let fila = tbody.insertRow();
-//         fila.insertCell().innerHTML = listar_tarjetas[index]['numerotarjeta'];
-//         fila.insertCell().innerHTML = listar_tarjetas[index]['fechavencimiento'];
-//         fila.insertCell().innerHTML = listar_tarjetas[index]['codigocvv'];
-                
-//      }
 
     
 
 
-}
+
 
 mostrarlista();
 
