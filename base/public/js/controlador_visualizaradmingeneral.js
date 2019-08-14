28ec2cc4ce9c96262fd03526438c2eae47470435
rarebeatle <amoralesm@ucenfotec.ac.cr>
2019-08-14 00:50:11 -0600

    Commit Ari

--- base/public/js/controlador_visualizaradmingeneral.js
+++ base/public/js/controlador_visualizaradmingeneral.js
@@ -1,10 +1,9 @@
 'use strict';
 
 const urlParams = new URLSearchParams(window.location.search);
-const txt_usuario = document.querySelector('#txt_usuario');
+const txt_nombreusuario = document.querySelector('#txt_nombreusuario');
 const txt_contrasena = document.querySelector('#txt_contrasena');
 const txt_correo = document.querySelector('#txt_correo');
-const avatar = document.querySelector('#avatar');
 
 let _id = urlParams.get('_id');
 
@@ -12,13 +11,11 @@ let _id = urlParams.get('_id');
 let llenar_perfil = async () => {
     let usuario = await obtenerUsuarioPerfil(sessionStorage.getItem("id"));
     if (usuario) {
-        let img = document.createElement("img");
-        img.src = usuario.avatar;
-        img.classList.add("avatar-admin");
-        avatar.innerHTML = img;
-        txt_usuario.innerHTML = usuario['usuario'];
-        txt_contrasena.innerHTML = usuario['contraseña'];
+    
+        txt_nombreusuario.innerHTML = usuario['nombreUsuario'];
         txt_correo.innerHTML = usuario['correo'];
+        txt_contrasena.innerHTML = usuario['contrasena'];
+       
     }
 };
 
