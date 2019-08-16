a526b279f2a18fe07bfd3d75a3425357d707afdb
rarebeatle <amoralesm@ucenfotec.ac.cr>
2019-08-16 14:02:26 -0600

    Commit Ariana

--- base/public/js/controlador_listar_generos.js
+++ base/public/js/controlador_listar_generos.js
@@ -1,47 +1,65 @@
-const tbody = document.querySelector('#tbl_tarjetas tbody');
-let listar_tarjetas = [];
+const tbody = document.querySelector('#tbl_generos tbody');
+let lista_genero = [];
+txt_filtro=document.querySelector('#txt_filtro');
 
 let mostrarlista = async () => {
-        listar_tarjetas = await obtenerTarjetas();
-        listar_tarjetas = listar_tarjetas.reverse();
+        lista_genero = await listarGenero();
+        lista_genero = lista_genero.reverse();
 
 
 
 tbody.innerHTML = '';
 
-for (let index = 0; index < listar_tarjetas.length; index++) {
+for (let index = 0; index < lista_genero.length; index++) {
     let fila = tbody.insertRow();
-    fila.insertCell().innerHTML = listar_tarjetas[index]['numerotarjeta'];
-    fila.insertCell().innerHTML = listar_tarjetas[index]['fechavencimiento'];
-    fila.insertCell().innerHTML = "***";
+    fila.insertCell().innerHTML = lista_genero[index]['genero'];
+
     
            
+    let estilos_modificar = document.createElement('img');
+    estilos_modificar.setAttribute('src', './imgs/edit-icon.png')
 
+    let celda_modificar = fila.insertCell();
+    let modificar = document.createElement('button');
+    modificar.type = 'button';
 
-//     listar_tarjetas = await obtenerTarjetas();
+    modificar.dataset._id = lista_genero[index]['_id'];
 
-let estilos_modificar = document.createElement('img');
-        estilos_modificar.setAttribute('src', './imgs/edit-icon.png')
+    celda_modificar.appendChild(modificar);
+    modificar.appendChild(estilos_modificar);
 
-        let celda_modificar = fila.insertCell();
-        let modificar = document.createElement('button');
-        modificar.type = 'button';
+    modificar.addEventListener('click', function () {
+        localStorage.setItem("modificarGenero", JSON.stringify(lista_genero[index]));
+        window.location.href = 'modificar_genero.html'
+    })
 
-        modificar.dataset._id = listar_tarjetas[index]['_id'];
-
-        celda_modificar.appendChild(modificar);
-        modificar.appendChild(estilos_modificar);
-
-        modificar.addEventListener('click', function () {
-            localStorage.setItem("modificarTarjeta", JSON.stringify(listar_tarjetas[index]));
-            window.location.href = 'modificar_tarjeta.html'
-        })
     
+}
+
+}
+
+
+
+    
+    let filtrarlista = async () => {
+
+        let filtro = txt_filtro.value.toLowerCase();
+        tbody.innerHTML = '';
+    
+    
+        for (let index = 0; index < lista_genero.length; index++) {
+            if (lista_genero[index]['genero'].toLowerCase().includes(filtro)) {
+                let fila = tbody.insertRow();
+                fila.insertCell().innerHTML = lista_genero[index]['genero'];
+      
         
-}
+    
+    }
 
-}
+        }
+    }
 
+window.addEventListener('load', mostrarlista);
+txt_filtro.addEventListener('keyup', filtrarlista);
 
-        window.addEventListener('load', mostrarlista);
-
+    
