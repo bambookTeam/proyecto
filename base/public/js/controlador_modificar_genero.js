1510770292f22c04192d568009cd07a41ad27513
rarebeatle <amoralesm@ucenfotec.ac.cr>
2019-08-16 14:36:30 -0600

    Commit

--- base/public/js/controlador_modificar_genero.js
+++ base/public/js/controlador_modificar_genero.js
@@ -1,33 +1,23 @@
 'use strict';
 
 
-var  modificar = document.getElementById('btn-modificar'),
-input_genero = document.getElementById('txt-genero');
+const modificar = document.getElementById('btn-modificar');
+ var input_genero = document.getElementById('txt-genero');
 
 const urlParams = new URLSearchParams(window.location.search);
 let _id = urlParams.get('_id');
 
 let cargar_formulario = async () => {
-    let libro = await obtener_libroId(_id);
-    if (libro) {
-        input_titulo.value = libro['titulo'];
-        input_edicion.value = libro['edicion'];
-        input_editorial.value = libro['editorial'];
-        input_autor.value = libro['autor'];
-        input_anno.value = libro['año'];
-        input_idioma.value = libro['idioma'];
-        input_isbn.value = libro['isbn'];
-        input_genero.value = libro['genero'];
-        input_tipo.value = libro['tipo'];
-        input_cantidad.value = libro['cantidad'];
-        input_precio.value = libro['precio'];
-        imgLibro.value = libro['portada'];
+    let genero = await obtener_generoId(_id);
+    if (genero) {
+    
+        input_genero.value = genero['genero'];
     }
 
 }
-let editar_libro = () => {
-    modificar_libro(_id, ptitulo, pedicion, peditorial, pautor, panno, pidioma, pisbn, pimagen, pgenero, ptipo, pcantidad, pprecio);
+let editar_genero = () => {
+    modificarGenero(_id,pgenero);
 };
 
 cargar_formulario();
-boton_modificar.addEventListener('click', editar_libro);
+modificar.addEventListener('click', editar_genero);
--- base/public/js/servicio_genero.js
+++ base/public/js/servicio_genero.js
@@ -11,15 +11,28 @@ let registrarGenero = (pgenero) => {
     });
 };
 
+let obtener_generoId = async (_id) => {
+    try {
+        //fetch data from a url endpoint
+        const response = await axios({
+            method: 'get',
+            url: `http://localhost:4000/api/listar-generos_id/${_id}`,
+            responseType: 'json'
+        });
+        
+        return response.data.generosBD
+    } catch (error) {
+        // console.log(error);
+    }
+}
 let modificarGeneroServicio = (idContacto, pgenero) => {
-    console.log(pfecha);
     axios({
         method: 'post',
         url: 'http://localhost:4000/api/modificar_genero',
         responseType: 'json',
         data: {
             _id: idContacto,
-            genero: pgenero,
+            genero: pgenero
        
         }
     });
