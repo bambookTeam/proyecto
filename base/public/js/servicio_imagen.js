a0454fe770bdff271f71dda88115c9e722f85bc2
Axel Casco González <acascog@exitocr.com>
2019-08-21 00:12:35 -0600

    axel
    cambios varios

--- base/public/js/servicio_imagen.js
+++ base/public/js/servicio_imagen.js
@@ -75,3 +75,45 @@ $(function () {
     };
 
 });
+
+
+
+
+
+//servicio foto perfil Autor
+$(function () {
+    let imagenUrl = '';
+
+    //ConfiguracionCloudinary
+    //with credentials available on
+    // your cloudinary account dashboard
+    $.cloudinary.config({ cloud_name: 'bambooks', api_key: '938645956677954' });
+
+    //Upload button1
+    let uploaButton = $('#btn_seleccionar_foto_perfil');
+    
+
+    //Upload button event
+    uploaButton.on('click', function (e) {
+        //Initiate upload
+        cloudinary.openUploadWidget({ cloud_name: 'bambooks', upload_preset: 'Bambooks', tags: ['cgal'] },
+            function (error, result) {
+                if (error) console.log(error);
+                //if no Error, log img data to console
+                let id = result[0].public_id;
+                console.log(id);
+                imagenUrl = 'https://res.cloudinary.com/bambooks/image/upload/' + id;
+                document.querySelector('#autor_foto_preview').src = imagenUrl;
+                console.log(imagenUrl);
+                
+            });
+    });
+
+    function processImage(id) {
+        let options = {
+            client_hints: true,
+        };
+        return $.cloudinary.url(id, options);
+    };
+
+});
